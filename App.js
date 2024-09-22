f4ee24f1aa30428d44a103c445a768a80c5e9f10
JetiTheS <jetro.erakangas@myy.haaga-helia.fi>
2024-09-22 14:30:16 +0300

    Osoite tehtävä tehty

--- App.js
+++ App.js
@@ -1,10 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert, TextInput, Button } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
+import { useEffect, useState } from 'react';
+import * as Location from 'expo-location';
+
 
 export default function App() {
+
+  const [keyword, setKeyword] = useState('');
+  const [location, setLocation] = useState(null);
+  const [region, setRegion] = useState({
+    latitude: 60.200692,
+    longitude: 24.934302,
+    latitudeDelta: 0.0322,
+    longitudeDelta: 0.0221,
+  })
+
+  const apikey = "66ed31c1c9fd4490011665mzge94039";
+
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== 'granted') {
+      Alert.alert('No permission to get location')
+      return;
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+
+    setRegion({
+      ...region,
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+    });
+  }
+
+  useEffect(() => { getLocation() }, []);
+
+  const handleAddress = () => {
+    fetch(`https://geocode.maps.co/search?q=${keyword}&api_key=${apikey}`)
+      .then(response => {
+        if (!response.ok)
+          throw new Error("Error in fetch:" + response.statusText);
+
+        return response.json()
+      })
+      .then(data => setRegion({
+        ...region,
+        latitude: parseFloat(data[0].lat),
+        longitude: parseFloat(data[0].lon),
+      }))
+      .catch(err => console.error(err));
+
+  }
+  //console.log(region);
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView
+        style={styles.map}
+        region={region}>
+        <Marker
+          coordinate={{
+            latitude: region.latitude,
+            longitude: region.longitude
+          }}
+          title='Haaga-Helia'
+        />
+      </MapView>
+      <TextInput
+        style={styles.text}
+        value={keyword}
+        onChangeText={text => setKeyword(text)}
+      />
+      <View style={styles.button}>
+        <Button title='SHOW' onPress={handleAddress} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +88,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    width: "100%",
+    marginBottom: 10
+
+  },
+  map: {
+    width: '100%', height: '100%'
+  },
+  button: {
+    width: "100%",
+    marginBottom: 150
+  }
 });
